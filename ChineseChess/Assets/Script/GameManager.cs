03c9cb02d49a3f641a80b140a839da05a43e6ee6
daveho8888 <daveho8888@gmail.com>
2017-03-01 20:47:05 -0800

    add new scene for game manager/men

---
+++ ChineseChess/Assets/Script/GameManager.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+	public static GameManager Instance{set; get;}
+
+	// Use this for initialization
+	private void Start () {
+		DontDestroyOnLoad(gameObject);	
+	}
+
+	public void ConnectButton(){
+		Debug.Log("Connect");
+	}
+
+	public void HostButton(){
+		Debug.Log("Host");
+	}
+}
