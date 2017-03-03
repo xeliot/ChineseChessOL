84d0f386cd407885d39011e43e070b8004eeadfe
daveho8888 <daveho8888@gmail.com>
2017-03-02 17:57:11 -0800

    test first build; communication successful!

--- ChineseChess/Assets/Script/GameManager.cs
+++ ChineseChess/Assets/Script/GameManager.cs
@@ -1,4 +1,6 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour {
 
@@ -7,6 +9,9 @@ public class GameManager : MonoBehaviour {
 	public GameObject mainMenu;
 	public GameObject serverMenu;
 	public GameObject connectMenu;
+
+	public GameObject serverPrefab;
+	public GameObject clientPrefab;
 
 	// Use this for initialization
 	private void Start () {
@@ -22,11 +27,32 @@ public class GameManager : MonoBehaviour {
 	}
 
 	public void HostButton(){
+
+		try
+		{
+			Server s = Instantiate(serverPrefab).GetComponent<Server>();
+			s.Init();
+		}
+		catch (Exception e)
+		{
+			Debug.Log(e.Message);
+		}
+
 		mainMenu.SetActive(false);
 		serverMenu.SetActive(true);
 	}
 	public void ConnectToServerButton(){
-
+		string hostAddress = GameObject.Find("HostInput").GetComponent<InputField>().text;
+		if(hostAddress == ""){hostAddress = "127.0.0.1";}
+		try
+		{
+			Client c = Instantiate(clientPrefab).GetComponent<Client>();
+			c.ConnectToServer(hostAddress, 6321);
+			connectMenu.SetActive(false);
+		}
+		catch (Exception e){
+			Debug.Log(e.Message);
+		}
 	}
 	public void BackButton(){
 		mainMenu.SetActive(true);
